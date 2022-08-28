ffce762934675823a79547eef6e23e2cb0a5070e
TatyanaErofeeva <pupchik@mail.ru>
2022-08-28 13:03:07 +0000

    Рефакторинг кода

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import SiteMenuView from './view/site-menu-view .js';
 import FilterView from './view/filter-view.js';
 import { render, RenderPosition } from './render.js';
-import PointsPresenter from './presenter/points-presenter.js';
+import BoardPresenter from './presenter/points-presenter.js';
 import PointsModel from './model/points-model.js';
 
 const headerMain = document.querySelector('.trip-main');
 const tripFilterContainer = headerMain.querySelector('.trip-controls__filters');
-const pointsModel = new PointsModel();
-const pointsPresenter = new PointsPresenter();
+const tripEvents = document.querySelector( '.trip-events' );
 
+const boardPresenter = new BoardPresenter();
+const pointsModel = new PointsModel();
 render (new SiteMenuView(), headerMain, RenderPosition.AFTERBEGIN);
 render (new FilterView(), tripFilterContainer, RenderPosition.AFTERBEGIN);
-
-pointsPresenter.init(headerMain, pointsModel);
+boardPresenter.init( tripEvents, pointsModel );
