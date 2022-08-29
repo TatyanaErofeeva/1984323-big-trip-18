1eec451ae8ebda8fff44afe442c0aca6b222576b
TatyanaErofeeva <pupchik@mail.ru>
2022-08-29 07:44:58 +0000

    Наследование от абстрактного класса, подписка на события.

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,4 +1,4 @@
-import {createElement} from '../render.js';
+import AbstractView from '../framework/view/abstract-view';
 
 const createFilterTemplate = () => (
   `<form class="trip-filters" action="#" method="get">
@@ -21,23 +21,10 @@ const createFilterTemplate = () => (
 </form>`
 );
 
-export default class FilterView {
-  #element = null;
+export default class FilterView extends AbstractView {
+
 
   get template() {
     return createFilterTemplate();
   }
-
-  get element() {
-    if(!this.#element){
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
-  }
-
 }
